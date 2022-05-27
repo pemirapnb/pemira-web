73c09d1728ee1895889956862614e62b19968f85
adearta <bagusmadearta01@gmail.com>
2022-05-27 11:40:49 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# PEMIRA KBM PNB 2021
+
+## _kenyel cang megae kene_
+
+Install package dari Composer dengan cara
+
+```sh
+composer install
+```
+
+Install package dari NodeJS dan build dengan cara.
+
+```sh
+npm i
+node run dev
+```
---
+++ resources/views/tailwinds.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta charset="UTF-8" />
+   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+   <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <title>Document</title>
+
+    
+</head>
+<body>
+    <div class="flex justify-center items-center w-screen h-screen"><h1 class="text-3xl text-gray-700">The Journey Start <span class="font-bold">Here</span> </h1>
+    </div>
+</body>
+</html>
